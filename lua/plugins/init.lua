bc49d8ac457e1ff3e76118e9daaf378e49a472fa
matthias <113443582+schachmatthias@users.noreply.github.com>
2024-07-25 21:43:50 +0200

    adds autocomplete

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,10 +25,19 @@ return {
   		},
   	},
   },
-  {
-    "neoclide/coc.nvim",
+
+{
+    "Exafunction/codeium.nvim",
+    requires = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("codeium").setup({
+        })
+    end
   },
-  --
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
