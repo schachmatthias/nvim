e884e409ae10718f06ba7afbac7300ba731e22f4
MaMo <113443582+schachmatthias@users.noreply.github.com>
2024-08-04 20:15:08 +0200

    adds deno language server and configures it

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,9 +1,9 @@
 -- EXAMPL
--- local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_attach = require("nvchad.configs.lspconfig").on_attach
 -- local on_init = require("nvchad.configs.lspconfig").on_init
 -- local capabilities = require("nvchad.configs.lspconfig").capabilities
 --
--- local lspconfig = require "lspconfig"
+local lspconfig = require "lspconfig"
 -- local servers = { "html", "cssls", "t" }
 
 -- lsps with default config
@@ -27,35 +27,75 @@
 --             },
 --           }
 --
-require'lspconfig'.tsserver.setup{
- filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-          init_options = {
-            plugins = {
-              {
-                name = '@vue/typescript-plugin',
-                location = require('mason-registry').get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server',
-                languages = { 'vue' },
-              },
-            },
-          },
-          handlers = {
-            -- Usually gets called after another code action
-            -- https://github.com/jose-elias-alvarez/typescript.nvim/issues/17
-            ['_typescript.rename'] = function(_, result)
-              return result
-            end,
-            -- 'Go to definition' workaround
-            -- https://github.com/holoiii/nvim/commit/73a4db74fe463f5064346ba63870557fedd134ad
-            ['textDocument/definition'] = function(err, result, ...)
-              result = vim.islist(result) and result[1] or result
-              vim.lsp.handlers['textDocument/definition'](err, result, ...)
-            end,
-          },
-        }
-        --[[ cfg_lazy_lsp_servers_volar ]]
-        volar = {}
+require("lspconfig").denols.setup {
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+  init_options = {
+    lint = true,
+    unstable = true,
+    suggest = {
+      imports = {
+        hosts = {
+          ["https://deno.land"] = true,
+          ["https://cdn.nest.land"] = true,
+          ["https://crux.land"] = true,
+        },
+      },
+    },
+  },
 
-require 'lspconfig'.eslint.setup {
+  on_attach = on_attach,
+}
+require("lspconfig").tsserver.setup {
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = require("mason-registry").get_package("vue-language-server"):get_install_path()
+          .. "/node_modules/@vue/language-server",
+        languages = { "vue" },
+      },
+    },
+  },
+  handlers = {
+    -- Usually gets called after another code action
+    -- https://github.com/jose-elias-alvarez/typescript.nvim/issues/17
+    ["_typescript.rename"] = function(_, result)
+      return result
+    end,
+    -- 'Go to definition' workaround
+    -- https://github.com/holoiii/nvim/commit/73a4db74fe463f5064346ba63870557fedd134ad
+    ["textDocument/definition"] = function(err, result, ...)
+      result = vim.islist(result) and result[1] or result
+      vim.lsp.handlers["textDocument/definition"](err, result, ...)
+    end,
+  },
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    vim.keymap.set("n", "<leader>ro", function()
+      vim.lsp.buf.execute_command {
+        command = "_typescript.organizeImports",
+        arguments = { vim.fn.expand "%:p" },
+      }
+    end, { buffer = bufnr, remap = false })
+  end,
+  root_dir = function(filename, bufnr)
+    local denoRootDir = lspconfig.util.root_pattern("deno.json", "deno.json")(filename)
+    if denoRootDir then
+      -- print('this seems to be a deno project; returning nil so that tsserver does not attach');
+      return nil
+      -- else
+      -- print('this seems to be a ts project; return root dir based on package.json')
+    end
+
+    return lspconfig.util.root_pattern "package.json"(filename)
+  end,
+  single_file_support = false,
+}
+--[[ cfg_lazy_lsp_servers_volar ]]
+volar = {}
+
+require("lspconfig").eslint.setup {
   filetypes = {
     "javascript",
     "javascriptreact",
@@ -68,18 +108,18 @@ require 'lspconfig'.eslint.setup {
     "astro",
     "json",
     "jsonc",
-  }
-}
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = {
-    '*.tsx',
-    '*.ts',
-    '*.jsx',
-    '*.js',
-    '*.cjs',
-    '*.mjs',
-    '*.vue',
-    '*.json',
   },
-  command = 'silent! EslintFixAll',
+}
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = {
+    "*.tsx",
+    "*.ts",
+    "*.jsx",
+    "*.js",
+    "*.cjs",
+    "*.mjs",
+    "*.vue",
+    "*.json",
+  },
+  command = "silent! EslintFixAll",
 })
