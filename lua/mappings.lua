f15fd2c5d9ba2110e8a2870aefeb5b6ba2ee6f1f
MaMo <113443582+schachmatthias@users.noreply.github.com>
2025-11-10 12:18:13 +0100

    custom keybinds, and removale of default ones

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,16 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
 
+map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", { desc = "Git commits" })
+map("n", "<leader>gb", "<cmd>Gitsigns blame_line<CR>", { desc = "Git blame" })
+map("n", "<leader>gd", "<cmd>DiffviewOpen<CR>", { desc = "Git diff" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- 🧹 Remove default <leader>tc mapping after everything loads
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    pcall(vim.keymap.del, "n", "<leader>cm")
+  end,
+})
