912c473371ad8c25db711bdb801c15d645385feb
MaMo <113443582+schachmatthias@users.noreply.github.com>
2025-11-14 18:36:11 +0100

    adds custom keymap which search and replaces all files project wide (so nice)

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -25,3 +25,43 @@ map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", { desc = "Git commits" 
 map("n", "<leader>gb", "<cmd>Gitsigns blame_line<CR>", { desc = "Git blame" })
 map("n", "<leader>gB", "<cmd>Telescope git_branches<CR>", { desc = "Git branches" })
 map("n", "<leader>gd", "<cmd>DiffviewOpen<CR>", { desc = "Git diff" })
+
+vim.keymap.set("n", "<leader>sr", function()
+  local from = vim.fn.input "Search: "
+  if from == "" then
+    return
+  end
+
+  local to = vim.fn.input "Replace: "
+  if to == "" then
+    return
+  end
+
+  local cmd = table.concat({
+    "rg -l",
+    vim.fn.shellescape(from),
+    -- exclude files:
+    "--glob '!node_modules/**'",
+    "--glob '!package.json'",
+    "--glob '!package-lock.json'",
+    "--glob '!yarn.lock'",
+    "--glob '!pnpm-lock.yaml'",
+    ".",
+  }, " ")
+
+  local files = vim.fn.systemlist(cmd)
+
+  if #files == 0 then
+    print "No matches found."
+    return
+  end
+
+  for _, file in ipairs(files) do
+    vim.cmd("silent edit " .. vim.fn.fnameescape(file))
+    vim.cmd(string.format([[silent! %%s/%s/%s/ge]], from, to))
+    vim.cmd "silent update"
+    vim.cmd "silent bdelete"
+  end
+
+  print("Done: replaced in " .. #files .. " files.")
+end, { desc = "Project Replace (excluding node_modules & lockfiles)" })
